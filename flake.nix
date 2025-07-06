52cf30573f08c797d3be1fb0d26a1e113125e65a
Brakenium <brake@brakenium.xyz>
2025-07-06 18:38:01 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Stylix options.json generator";
+
+  inputs.stylix.url = "github:nix-community/stylix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+
+  outputs =
+    {
+      nixpkgs,
+      stylix,
+      home-manager,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      makeOptionsDoc =
+        configuration:
+        pkgs.nixosOptionsDoc {
+          inherit (configuration) options;
+        };
+    in
+    {
+      hmOptionsJSON =
+        (makeOptionsDoc (
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              stylix.homeModules.stylix
+              (stylix + "/doc/hm_compat.nix")
+              {
+                home = {
+                  homeDirectory = "/home/book";
+                  stateVersion = "22.11";
+                  username = "book";
+                };
+              }
+            ];
+          }
+        )).optionsJSON;
+    };
+}
